506453be7957936db3dc72e3389efd9c8bab9d5e
Thomas Eizinger <thomas.eizinger@gmail.com>
2016-05-14 15:54:17 +0200

    allow modification of exception setting

--- src/main/java/com/github/thomaseizinger/oocl/OOCL.java
+++ src/main/java/com/github/thomaseizinger/oocl/OOCL.java
@@ -2,10 +2,19 @@ package com.github.thomaseizinger.oocl;
 
 import org.jocl.CL;
 
-// TODO needed?
 public final class OOCL {
 
+    private OOCL() { }
+
     static {
+        enableExceptions();
+    }
+
+    public static void enableExceptions() {
         CL.setExceptionsEnabled(true);
+    }
+
+    public static void disableExceptions() {
+        CL.setExceptionsEnabled(false);
     }
 }
